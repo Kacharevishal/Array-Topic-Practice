e1489ba5e58e06be0b2d8e814a683760b76762e0
Vishal Kachare <155882477+Kacharevishal@users.noreply.github.com>
2026-01-19 19:36:50 +0530

    Add files via upload

---
+++ ArrayDecOrder.java
@@ -0,0 +1,30 @@
+package practice19;
+
+class ArrayDecOrder {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 9, 1, 3};
+        int temp;
+        
+        for(int i = 0;i<arr.length;i++)
+        {
+        	    for(int j = i+1;j<arr.length;j++)
+        	    {
+        	    	    if(arr[i] > arr[j])
+        	    	    {
+        	    	    	    temp = arr[i];
+        	    	    	    arr[i] = arr[j];
+        	    	    	    arr[j] = temp;
+        	    	    }
+        	    }
+        }
+        for(int num : arr)
+        {
+        	   System.out.print(num+" ");
+        }
+        
+    }
+}
+
+
+
